1d963e74e79ab353aeec919c5ac79fc37f730ca3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,38 @@
-output "data_factory_linked_service_odbcs" {
-  description = "All data_factory_linked_service_odbc resources"
-  value       = azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs
-  sensitive   = true
-}
 output "data_factory_linked_service_odbcs_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.additional_properties }
 }
 output "data_factory_linked_service_odbcs_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.annotations }
 }
 output "data_factory_linked_service_odbcs_basic_authentication" {
-  description = "List of basic_authentication values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.basic_authentication]
+  description = "Map of basic_authentication values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.basic_authentication }
   sensitive   = true
 }
 output "data_factory_linked_service_odbcs_connection_string" {
-  description = "List of connection_string values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.connection_string]
+  description = "Map of connection_string values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.connection_string }
 }
 output "data_factory_linked_service_odbcs_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.data_factory_id }
 }
 output "data_factory_linked_service_odbcs_description" {
-  description = "List of description values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.description]
+  description = "Map of description values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.description }
 }
 output "data_factory_linked_service_odbcs_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_odbcs_name" {
-  description = "List of name values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.name]
+  description = "Map of name values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.name }
 }
 output "data_factory_linked_service_odbcs_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_odbcs"
-  value       = [for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_odbcs, keyed the same as var.data_factory_linked_service_odbcs"
+  value       = { for k, v in azurerm_data_factory_linked_service_odbc.data_factory_linked_service_odbcs : k => v.parameters }
 }
 
